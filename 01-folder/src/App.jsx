8d1d3456d9ff080eddd37e540196e860c7c0aab4
CodeByMehra <kpvishalmehra@gmail.com>
2026-05-04 18:28:10 +0530

    card compornent imported

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import second from './components/card'
 
 const app = () => {
   return (
     <div className="card">
       <h1>Mehra's Card</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, odio.</p>
+
+      {Card()}
     </div>
+
+    
   )
 }
